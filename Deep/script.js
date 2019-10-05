b25ed4f4ee1636646683f01f118df8c1d31af526
Forgetfull <oksana14888@gmail.com>
2019-10-05 16:05:37 +0500

    add js Event

--- Deep/script.js
+++ Deep/script.js
@@ -1,4 +1,23 @@
 // событие нажате на клавишу
 // событие комбинация клавиш
 // id всех кнопок 
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
+const btn9 = document.getElementById('btn9');
 // функ воис
+function voice(){
+	alert("hello word");
+}
+
+btn4.addEventListener('click', () => {alert("asdf")});
+
+
+document.onkeypress = function zaz(event){
+	console.log(event);
+}
---
+++ Events/script.js
@@ -0,0 +1,55 @@
+//Обязательно к событиюдобавляем On
+/*document.onkeypress = function zaz(event){
+	console.log(event);
+}
+*/
+// функция горячей клавиши.
+/*document.addEventListener("keypress", function(e){
+	// q - 113,w - 119, e - 101
+	console.log(e.keyCode);
+	if(e.keyCode == 113){
+		console.log(e.key)
+	}
+});
+*/
+
+let flag = false;
+document.onkeydown = function(e){
+	console.log(e.code);
+	if (e.code == 'AltLeft') flag = true;
+	if (e.code == 'keyN' && flag){
+		flag = false;
+		console.log('work');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+// событие нажате на клавишу
+// событие комбинация клавиш
+// id всех кнопок 
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+const btn6 = document.getElementById('btn6');
+const btn7 = document.getElementById('btn7');
+const btn8 = document.getElementById('btn8');
+const btn9 = document.getElementById('btn9');
+// функ воис
+function voice(){
+	alert("hello word");
+}
+
+btn4.addEventListener('click', () => {alert("asdf")});
+
+
+
